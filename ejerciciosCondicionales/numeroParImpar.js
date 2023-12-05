ad51c8b93a8443ef5bdc515d34d32373f1bad435
Lalo Montiel <136762830+Lalo0Montiel@users.noreply.github.com>
2023-12-05 13:13:57 -0600

    Se añadió ejercicio numero par o impar

---
+++ ejerciciosCondicionales/numeroParImpar.js
@@ -0,0 +1,7 @@
+// Crear una función que evalúe si un número es par o impar //
+
+const numParImopar = (n) => {
+    (n % 2 === 0) ? console.log(`EL número ${n} es par`) : console.log(`EL número ${n} es impar`);
+}
+
+let numero = numParImopar(7)
